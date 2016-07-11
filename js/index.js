b83d7610b39800a5229dc518e0a696b786eb57d3
litichevskiy <kunin25@ukr.net>
2016-07-11 20:07:07 +0300

    Tab

--- js/index.js
+++ js/index.js
@@ -1,9 +1,9 @@
 (function(){
 
-	var str = '(-3)';
+	var str = '3+5-(8+1)';
 
 	var calc = new Calculator();
 	console.log( calc.dataValidation( str ) );
-	console.log( calc.getSumInQuotes( str ) );
+  console.log( calc.getSumInQuotes( str ) );
 
 })();
