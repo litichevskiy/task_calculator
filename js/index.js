8f8c2a360cf19e573de746b1a0b06ebfa98dd855
litichevskiy <kunin25@ukr.net>
2016-09-10 01:38:35 +0300

    .

--- js/index.js
+++ js/index.js
@@ -1,15 +1,43 @@
 (function(){
 
-	var calculator = new Calculator();
+	var moduleCalculator = new Calculator(),
+        calculator = $('.container_calc'),
+        input = $('.field_for_text'),
+        result;
+
 
     try{
 
-        console.log( 'RESULT = ', calculator.calculateSum( '5+6+((2-2)+(8-2))' ) );
+        console.log( 'RESULT = ', moduleCalculator.calculateSum( '5+6+((2-2)+(8-2))' ) );
 
     } catch( error ) {
 
         console.info( error.message )
 
     }
+
+    $(calculator).click(function(event) {
+
+        if ( !moduleCalculator ) {
+            throw new Error('calculator required module calculator !');
+        }
+
+        var targetValue = event.target.dataset.val;
+
+        if( !targetValue ) return;
+
+            if( targetValue === 'clear' ) {
+                $(input).val('');
+                $(input).focus();
+            } else
+                if ( targetValue === '=' ) {
+                     try{
+                        result = moduleCalculator.calculateSum( $(input).val() );
+                        $(input).val( result['result'] );
+                    } catch( error ) {
+                        console.info( error.message )
+                    }
+                } else  $(input).val( $(input).val() + targetValue );
+    });
 
 })();
